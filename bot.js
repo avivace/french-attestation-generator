bd01ad19dc58ea58fa1ccc7db4f477ab210dfcdb
Antonio Vivace <avivace4@gmail.com>
2020-11-16 02:14:55 +0100

    Basic /start and /help showing payload examples

--- bot.js
+++ bot.js
@@ -3,6 +3,43 @@ const gen = require('./index')
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
+bot.start((ctx) => ctx.reply('Welcome. Source code: https://github.com/avivace/french-attestation-generator'))
+
+bot.help((ctx)=>{
+    ctx.reply(`Valid reasons:
+  travail
+  achats
+  sante
+  famille
+  handicap
+  sport_animaux
+  convocation
+  missions
+  enfants
+
+  Example JSON:
+
+  {
+  "firstname": "Antuzzo",
+  "lastname": "surname",
+  "birthday": "01/01/1991",
+  "placeofbirth": "Wien",
+  "address": "palceholder",
+  "city": "placeholder",
+  "zipcode": "01234",
+  "datesortie": "13/11/2020",
+  "heuresortie": "18:11",
+  "reasons" : "sport_animaux"
+  }
+
+
+  More than one reason:
+
+  "reasons" : "sport_animaux, enfants"
+
+  `)
+})
+
 bot.on('message', (ctx) => {
     obj = null
 
@@ -34,5 +71,6 @@ bot.on('message', (ctx) => {
 
 
 })
+
 
 bot.launch()
