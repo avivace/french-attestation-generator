9b29ad568f3a0e5098bb109d5b94003b8edf0b8e
Antonio Vivace <avivace4@gmail.com>
2020-11-17 18:45:57 +0100

    Fix /reasons

--- bot.js
+++ bot.js
@@ -70,14 +70,12 @@ bot.command('reasons', ctx => {
             }
         }
         ctx.reply(
-            reasons.length ? reasons : `Invalid reason. Following are valid reasons: ${Object.keys(gen.tableOfReasons).join(', ')}`
+            reasons.length ? reasons.join('\n') : `Invalid reason. Following are valid reasons: ${Object.keys(gen.tableOfReasons).join(', ')}`
         );        
     } catch (err) {
         ctx.reply('Error '+ err.message);
     }
-    ctx.reply(
-        reasons.length ? reasons : `Invalid reason. Following are valid reasons: ${Object.keys(gen.tableOfreasons).join(', ')}`
-    );
+
 });
 
 bot.command('template', ctx => {
